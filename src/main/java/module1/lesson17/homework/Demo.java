777a93c7323bd17fd2cab6802c9a1ae19b94bc8c
andrii_shylin <andrii_shylin@gmail.com>
2025-12-11 18:02:24 +0100

    homework lesson 17

---
+++ src/main/java/module1/lesson17/homework/Demo.java
@@ -0,0 +1,7 @@
+package module1.lesson17.homework;
+
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/module1/lesson17/homework/List.java
@@ -0,0 +1,13 @@
+package module1.lesson17.homework;
+
+public interface List<E>  {
+    int size();
+
+    void add(E element);
+
+    void addByIndex(E element, int index);
+
+    E getByIndex(int index);
+
+    void clear();
+}
